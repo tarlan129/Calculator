5ba5f63ddd440018fb63cfc9611907bb0bc78ab0
tarlan129 <tarlan129@gmail.com>
2026-06-19 06:15:36 +0400

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function divide(a, b) {
     return a / b;
 }
 
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+ 
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Invalid operator';
+    }   
+}
+console.log(operate(3, '-', 5));
